33d8bca9540863a5a46c652ec1c2a9fd690022b3
Alexis <a.goutorbe@gmail.com>
2021-06-20 14:51:18 +0900

    second

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import Express from "express";
+
 import dotenv from "dotenv";
 import initCron from "./app.js";
 import cronJobInit from "./worker/index.js";
 
-dotenv.config();
-const app = Express();
-const port = process.env.PORT || 3001
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
